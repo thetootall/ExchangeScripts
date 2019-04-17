bc5ec95fb5390dc907105b19ed7f55fc3ee8dca2
thetootall <thetootall@gmail.com>
2019-04-17 14:44:21 -0500

    Update EnableExchCert.ps1

--- EnableExchCert.ps1
+++ EnableExchCert.ps1
@@ -1,5 +1,5 @@
 #Automatically enables Exchange Certificate based on subject name
-#For updates and issues: 
+#For updates and issues: https://github.com/thetootall/ExchangeScripts/blob/master/EnableExchCert.ps1
 $varurl = Read-Host "Enter Certificate Subject"
 $varsub = "CN=" + $varurl
 $varthumb = Get-ChildItem -Recurse Cert:\LocalMachine\My | Where { $_.subject -eq $varsub -and $_.NotAfter -gt (Get-Date).AddDays(0) } | Select Thumbprint
