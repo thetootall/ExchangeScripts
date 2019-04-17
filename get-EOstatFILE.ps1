9ac1a0a935c93840a55e944aee5cfaa6a087ef99
thetootall <thetootall@gmail.com>
2019-04-16 19:38:09 -0500

    Add files via upload

---
+++ get-EOstatFILE.ps1
@@ -0,0 +1,33 @@
+﻿
+$logfile = "_MRLL_MAILBOXSTATS_REPORT2.txt"
+#$Users = Get-Mailbox -resultsize unlimited
+$Users = import-csv failedusers.csv
+
+$header = "Mail|Name|Type|LastLogon|LastLogoff|ItemSize|ItemCount|DeletedItemSize|DeletedItemCount"
+$header | Out-file $logfile -Append
+
+Foreach ($item in $Users){
+
+$myuser = $item.UserPrincipalName
+$mymailbox = Get-mailbox $myuser
+
+$mymail = $mymailbox.PrimarySMTPAddress
+$mymailid = $mymailbox.DistinguishedName
+$mymailtype = $mymailbox.RecipientType
+
+$mymailboxstat = Get-MailboxStatistics $mymailid | select userprincipalname,displayname,RecipientType,lastlogontime,lastlogofftime,ItemCount,TotalItemSize,TotalDeletedItemSize,DeletedItemCount
+
+$mymaildisp = $mymailboxstat.displayname
+$mymaillogon = $mymailboxstat.lastlogontime
+$mymaillogoff = $mymailboxstat.lastlogofftime
+$mymailsizeitem = $mymailboxstat.ItemCount
+$mymailsizedeleted = $mymailboxstat.DeletedItemCount
+
+$mymailitem = [math]::Round(($mymailboxstat.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1MB),2)
+$mymaildeleted = [math]::Round(($mymailboxstat.TotalDeletedItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1MB),2)
+
+$output = "$mymail|$mymaildisp|$mymailtype|$mymaillogon|$mymaillogoff|$mymailitem|$mymailsizeitem|$mymaildeleted|$mymailsizedeleted"
+
+Write-host $Output
+$output | Out-file $logfile -Append
+}
