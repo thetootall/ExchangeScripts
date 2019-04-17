0f44dbfc3efbf2af3cdfb08eaf05c69819b19579
thetootall <thetootall@gmail.com>
2019-04-17 10:05:56 -0500

    Update EXGAudit.ps1

--- EXGAudit.ps1
+++ EXGAudit.ps1
@@ -1,11 +1,18 @@
-#Be sure Powershell Remoting is enabled
-#To enable on each server run winrm quickconfig
+Write-host "Please save this file as exch.ps1 on one of your Exchange servers under a new output folder (IE C:\exgstats)" -BackgroundColor White -ForegroundColor Red
+Write-host "Launch Exchange Management Shell and run exch.ps1" -BackgroundColor White -ForegroundColor Red
+Write-host "ZIP the output folder and email back to Concurrency" -BackgroundColor White -ForegroundColor Red
+Write-host " "
+Write-host "Be sure Powershell Remoting is enabled on all Exchange servers." -BackgroundColor White -ForegroundColor Red
+Write-host "If not, the first step, "Gathering Exchange Server build info", will fail. All other scripts should run." -BackgroundColor White -ForegroundColor Red
+Write-host "To enable on each server run winrm quickconfig" -BackgroundColor White -ForegroundColor Red
+Write-host " "
+Pause
 
-#Please save this file as exch.ps1 on one of your Exchange servers and save under C:\temp
-#Launch Exchange Management Shell and run exch.ps1
-#ZIP the output folder and email back to Concurrency
+PAUSE
 
 #Get Server Versions
+Write-host "Gathering Exchange Server build info" -BackgroundColor Green -ForegroundColor Black
+
 $exchangeservers = Get-ExchangeServer
  
 $report = @()
@@ -52,6 +59,7 @@ Clear-variable installpath
 Clear-variable ver
 }
  
+Write-host "Pull Exchange Server URLs" -BackgroundColor Green -ForegroundColor Black
 $report | export-csv exchangeversions.csv -notype
 start-Transcript serverurls.txt
 #Get Server URLs
@@ -120,59 +128,59 @@ foreach ($i in $exchangeservers)
     }
 stop-transcript
 
-Write-Host "Pull mailbox + db sizes (mailbox report)"
+Write-Host "Pull mailbox + db sizes (mailbox report)" -BackgroundColor Green -ForegroundColor Black
 Get-Mailbox -Resultsize Unlimited | Get-MailboxStatistics | select-object DisplayName, alias, Database, {$_.TotalItemSize.Value.ToMB()}, ItemCount, {$_.TotalDeletedItemSize.Value.ToMB()}, DeletedItemCount, OrganizationalUnit, LastLogonTime | Export-CSV mbxDBsize.csv -notype
 
-Write-Host "Pull quotas, policies (policy report)"
+Write-Host "Pull quotas, policies (policy report)" -BackgroundColor Green -ForegroundColor Black
 Get-Mailbox -Resultsize Unlimited | select-object Displayname, Alias, PrimarySMTPAddress, UserPrincipalName, RecipientTypeDetails, OrganizationalUnit, UseDatabaseQuotaDefaults, EmailAddressPolicyEnabled, *Litigation*, InPlaceHolds, RetentionPolicy, ManagedFolderMailboxPolicy, WhenMailboxCreated | Export-CSV mbxPOLICYlist.csv -notype
 
-Write-Host "Pull all recipient types of Group (group)"
+Write-Host "Pull all recipient types of Group (group)" -BackgroundColor Green -ForegroundColor Black
 Get-Recipient -ResultSize Unlimited | ?{$_.RecipientType -like "*group*"} | select name, recipienttype, OrganizationalUnit, primarysmtpaddress | export-csv dllist.csv –notype
 
-Write-Host "Pull Remote IP ranges for Receive connectors (remoteips)"
+Write-Host "Pull Remote IP ranges for Receive connectors (remoteips)" -BackgroundColor Green -ForegroundColor Black
 Get-ReceiveConnector | Select server,name -expandproperty RemoteIPRanges | Sort Name | Export-CSV remoteip.csv –notype
 
-Write-Host "Pull Mailbox Full Access Permissions List"
+Write-Host "Pull Mailbox Full Access Permissions List" -BackgroundColor Green -ForegroundColor Black
 Get-Mailbox -Resultsize Unlimited | Get-MailboxPermission | where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.IsInherited -eq $false} | Select Identity,User,@{Name='Access Rights';Expression={[string]::join(', ', $_.AccessRights)}} | Export-Csv -NoTypeInformation mbxACLsource.csv
 
-Write-Host "Pull Mailbox Delegate Permissions List"
+Write-Host "Pull Mailbox Delegate Permissions List" -BackgroundColor Green -ForegroundColor Black
 Get-Mailbox -ResultSize unlimited | Get-CalendarProcessing | where { $_.ResourceDelegates -ne "" } | Select-Object identity,@{Name=’ResourceDelegates’;Expression={[string]::join(",", ($_.ResourceDelegates))}} | Export-csv -Path mbxResourceDelegates.csv 
 
-Write-Host "Pull User Archive Mailboxes"
+Write-Host "Pull User Archive Mailboxes" -BackgroundColor Green -ForegroundColor Black
 Get-Mailbox -Archive -Resultsize Unlimited | Get-MailboxStatistics | select-object DisplayName, alias, Database, {$_.TotalItemSize.Value.ToMB()}, ItemCount, {$_.TotalDeletedItemSize.Value.ToMB()}, DeletedItemCount, OrganizationalUnit, LastLogonTime | Export-CSV mbxARCsize.csv -notype
 
-Write-Host "Pull Mailbox Databases sizes + white space"
+Write-Host "Pull Mailbox Databases sizes + white space" -BackgroundColor Green -ForegroundColor Black
 Get-MailboxDatabase -Status | sort name | select name,@{Name='DB Size (Gb)';Expression={$_.DatabaseSize.ToGb()}},@{Name='Available New Mbx Space Gb)';Expression={$_.AvailableNewMailboxSpace.ToGb()}} | Export-csv mdb-size.csv –notype
 
-Write-Host "Pull Database Core Data"
+Write-Host "Pull Database Core Data" -BackgroundColor Green -ForegroundColor Black
 Get-MailboxDatabase | select Name, Server, JournalRecipient, OfflineAddressBook, PublicFolderDatabase, IsExcludedFromProvisioning, IsSuspendedFromProvisioning, EdbFilePath, LogFolderPath, CircularLoggingEnabled, rpcClientAccessServer, DeletedItesRetention | export-csv mbd-stat.csv -notype
 
-Write-Host "Pull DAG Name"
+Write-Host "Pull DAG Name" -BackgroundColor Green -ForegroundColor Black
 Get-DatabaseAvailabilityGroup | select name,*network*,*activation* | Export-Csv DAGinfo.csv –notype
 
-Write-Host "Pull DAG Network Settings"
+Write-Host "Pull DAG Network Settings" -BackgroundColor Green -ForegroundColor Black
 Get-DatabaseAvailabilityGroupNetwork | select name, MapiAccessEnabled | Export-csv DAGnetwork.csv -notype
 
-Write-Host "Pull activation preferences (DAG)"
+Write-Host "Pull activation preferences (DAG)" -BackgroundColor Green -ForegroundColor Black
 Get-MailboxDatabase | select name,@{Name=' ActivationPreference ';Expression={[string]::join(', ', $_.ActivationPreference)}}, MasterServerOrAvailabilityGroup | export-csv mbd-activation.csv -notype
 
-Write-Host "Pull ActiveSync Devices"
+Write-Host "Pull ActiveSync Devices" -BackgroundColor Green -ForegroundColor Black
 foreach ($user in $device) { Get-ActiveSyncDeviceStatistics -mailbox $user | Select-object identity, devicetype, Lastsuccesssync | Export-csv -encoding "unicode" -Path activesyncdevice.csv -NoType}
 
-Write-Host "Pull Exchange Certificates
+Write-Host "Pull Exchange Certificates" -BackgroundColor Green -ForegroundColor Black
 Get-ExchangeCertificate | Select @{Name='CertificateDomains';Expression={[string]::join(', ', $_.CertificateDomains)}}, HasPrivateKey, IsSelfSigned, Issuer, NotAfter, NotBefore, PublicKeySize, SerialNumber, Services, Status, Subject, Thumbprint | export-csv exg-cert.csv -notype
 
-Write-Host "Pull Public Folder Database Info"
+Write-Host "Pull Public Folder Database Info" -BackgroundColor Green -ForegroundColor Black
 Get-publicfolderdatabase | Select Identity,Alias,@{Name='Servers';Expression={[string]::join(', ', $_.Servers)}},EdbFilePath,LogFolderPath | Export-Csv pub-info.csv -notype
 
-Write-Host "Pull Public Folder Statistics"
+Write-Host "Pull Public Folder Statistics" -BackgroundColor Green -ForegroundColor Black
 Get-publicfolderstatistics -ResultSize Unlimited | select name,folderpath,lastaccesstime,lastmodificationtime, {$_.TotalItemSize.Value.ToKB()}, ItemCount, {$_.TotalDeletedItemSize.Value.ToKB()} | export-csv pub-stats.csv -notype
 
-Write-Host "Pull Retention Policies"
+Write-Host "Pull Retention Policies" -BackgroundColor Green -ForegroundColor Black
 Get-RetentionPolicy | select name,retentionpolicytaglinks | export-csv retention-policy.csv -notype
 Get-RetentionPolicyTag | select "Name","Description","RetentionEnabled","RetentionAction","AgeLimitForRetention","MoveToDestinationFolder","TriggerForRetention" | export-csv retention-tag.csv -NoTypeInformation
 
-Write-Host "Pull In-Place Hold Search Policies"
+Write-Host "Pull In-Place Hold Search Policies" -BackgroundColor Green -ForegroundColor Black
 Get-mailboxsearch | select Name,@{Name='SourceMailboxes';Expression={[string]::join(', ', $_.SourceMailboxes)}},@{Name='Sources';Expression={[string]::join(', ', $_.Sources)}},@{Name='PublicFolderSources';Expression={[string]::join(', ', $_.PublicFolderSources)}},AllPublicFolderSources,@{Name='SearchStatistics';Expression={[string]::join(', ', $_.SearchStatistics)}},Version,@{Name='TargetMailbox';Expression={[string]::join(', ', $_.TargetMailbox)}},@{Name='Target';Expression={[string]::join(', ', $_.Target)}},@{Name='SearchQuery';Expression={[string]::join(', ', $_.SearchQuery)}},Language,@{Name='Senders';Expression={[string]::join(', ', $_.Senders)}},@{Name='Recipients';Expression={[string]::join(', ', $_.Recipients)}},StartDate,EndDate,@{Name='MessageTypes';Expression={[string]::join(', ', $_.MessageTypes)}},IncludeUnsearchableItems,EstimateOnly,ExcludeDuplicateMessages,Resume,IncludeKeywordStatistics,KeywordStatisticsDisabled,PreviewDisabled,@{Name='Information';Expression={[string]::join(', ', $_.Information)}},StatisticsStartIndex,TotalKeywords,LogLevel,@{Name='StatusMailRecipients';Expression={[string]::join(', ', $_.StatusMailRecipients)}},Status,LastRunBy,LastStartTime,LastEndTime,NumberMailboxesToSearch,PercentComplete,ResultNumber,ResultNumberEstimate,ResultSize,ResultSizeEstimate,ResultSizeCopied,ResultsLink,PreviewResultsLink,@{Name='Errors';Expression={[string]::join(', ', $_.Errors)}},InPlaceHoldEnabled,ItemHoldPeriod,InPlaceHoldIdentity,ManagedByOrganization,@{Name='FailedToHoldMailboxes';Expression={[string]::join(', ', $_.FailedToHoldMailboxes)}},@{Name='InPlaceHoldErrors';Expression={[string]::join(', ', $_.InPlaceHoldErrors)}},Description,LastModifiedTime,KeywordHits,IsValid,ObjectState | export-csv mbx-search.csv -notype
 
 Write-Host "Data collection complete; please ZIP and return to consultant." -Foregroundcolor Yellow
