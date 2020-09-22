14731343ff2cc21b43ab02d29a1161eeed6e8eed
Chris Blackburn <thetootall@gmail.com>
2020-09-21 19:41:05 -0500

    Add files via upload

---
+++ upload-senderdomain.ps1
@@ -0,0 +1,30 @@
+﻿Param(
+   [Parameter(Mandatory=$True,Position=1)]
+   [string]$ruleName,
+  
+   [Parameter(Mandatory=$True)]
+   [string]$domainListFilePath
+)
+#Read the contents of the text file into an array
+[Array]$newsafeDomainList = Get-Content $domainListFilePath
+#If the rule already exists update the existing allowed sender domains, else create a new rule.
+if (Get-TransportRule $ruleName -EA SilentlyContinue)
+{
+  "Updating existing rule..."
+  [array]$safeDomainList = Get-TransportRule $ruleName |select -ExpandProperty SenderDomainIs
+  [array]$completeList = $safeDomainList + $newSafeDomainList
+  [array]$completeList = $completeList | select -uniq | sort
+  write-host $completelist
+  $ready = read-host "Press Y to commit"
+  If ($ready -eq "Y"){
+  set-TransportRule $ruleName -SenderDomainIs $completeList
+  }
+}
+else
+{
+  "Creating new rule..."
+  $newSafeDomainList = $newSafeDomainList | sort    
+  New-TransportRule $ruleName -SenderDomainIs $newSafeDomainList -SetSCL "-1"
+}
+
+Get-TransportRule $ruleName | fl SenderDomainIs
