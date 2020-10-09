6c82e6c7bf3c47f501026dbabd8ab1e8ae4397ac
Chris Blackburn <thetootall@gmail.com>
2020-10-08 19:23:04 -0500

    Add files via upload

---
+++ add-recipientpermissions.ps1
@@ -0,0 +1,18 @@
+#CSV will be formatted with two column headers: "email" with addresses of Shared mailboxes, and "group" with the name of the group
+#$file = Read-host "type file path (if not in current folder)"
+$csvlist = import-csv "grouplist.csv"
+ForEach ($item in $csvlist) {
+#start L1
+$e = $item.email
+$g = $item.group
+write-host "Adding permission $e adding $g"
+$mailboxID = Get-EXOMailbox -PrimarySmtpAddress $e
+$mailboxID1 = $mailboxID.Identity
+$agree = Read-host "Press Y to Continue"
+If ($agree -eq "Y"){
+#start L2
+Add-RecipientPermission -Identity $mailboxID1 -trustee $g -AccessRights SendAs -confirm:$false
+#end L2
+}
+#end L1
+}
