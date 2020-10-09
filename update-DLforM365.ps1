9a70824d9e71547eb905539de050e6319a6206ce
Chris Blackburn <thetootall@gmail.com>
2020-10-09 14:27:30 -0500

    Create update-DLforM365.ps1

---
+++ update-DLforM365.ps1
@@ -0,0 +1,11 @@
+#CSV will be formatted with two column headers: "email" with addresses of Shared mailboxes, and "group" with the name of the group
+#$file = Read-host "type file path (if not in current folder)"
+$csvlist = import-csv "grouplist.csv"
+ForEach ($item in $csvlist) {
+#start L1
+$g = $item.group
+write-host "Adding permission $e adding $g"
+$mailboxID = Enable-DistributionGroup $g
+Set-ADGroup -Identity $g -GroupScope Universal
+#end L1
+}
