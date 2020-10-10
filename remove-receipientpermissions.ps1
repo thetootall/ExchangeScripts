bef257add042ba5aa0b2e16926b8a2527f57eade
Chris Blackburn <thetootall@gmail.com>
2020-10-09 21:10:20 -0500

    Update remove-receipientpermissions.ps1

--- remove-receipientpermissions.ps1
+++ remove-receipientpermissions.ps1
@@ -6,10 +6,14 @@ Foreach ($m in $list){
 Write-host $m
 $m = $m.email
 [string]$mailboxID = Get-Mailbox -identity $m
+#Edit - you dont need the following for Exchange on-premise
 $mailboxID1 = $mailboxID.ExchangeGUID
 $mailboxID1 = $mailboxID1.guid
 write-host "Clearing $mailboxID"
-$sendperms = Get-RecipientPermission -Identity $mailboxID1 | ?{($_.Identity -eq $MailboxID) -and ($_.IsInherited -eq $False) -and -not ($_.Trustee -match "NT AUTHORITY")} |Select *
+#Exchange Online
+$sendperms = Get-RecipientPermission -Identity $mailboxID1 | ?{($_.Identity -eq $MailboxID) -and ($_.IsInherited -eq $False) -and -not ($_.Trustee -match "NT AUTHORITY")} | Select *
+#Exchange on-premise
+#$sendperms = Get-ADPermission -Identity $mailboxID | ?{($_.Identity -eq $MailboxID) -and ($_.IsInherited -eq $False) -and -not ($_.User -match "NT AUTHORITY")} | Select *
 $sendperms | fl
 $agree = Read-host "Press Y to Continue"
 If ($agree -eq "Y"){
@@ -19,7 +23,10 @@ Foreach ($i in $sendperms){
 $listuser = $i.trustee 
 $listidentity = $i.identity
 write-host "removing trustee $listuser"
+#Exchange Online
 Remove-RecipientPermission -Identity $listidentity -Trustee $listuser -AccessRights SendAs -confirm:$false
+#Exchange On-premise
+#Remove-ADPermission -Identity $listidentity -TUser $listuser -ExtendedRights "Send As" -confirm:$false
 #end L3
 }
 #end L2
