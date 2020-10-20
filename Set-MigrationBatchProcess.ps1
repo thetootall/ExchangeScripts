93bbeaa1d30e7aa7b688f23aea3bbb59f77b48b7
Chris Blackburn <thetootall@gmail.com>
2020-10-20 09:41:37 -0500

    Update Set-MigrationBatchProcess.ps1

--- Set-MigrationBatchProcess.ps1
+++ Set-MigrationBatchProcess.ps1
@@ -1,17 +1,20 @@
 #http://www.networknet.nl/apps/wp/archives/2932
 #https://docs.microsoft.com/en-us/powershell/module/exchange/new-migrationbatch?view=exchange-ps
-#be sure to structure your CSV with a column header "EmailAddress"
-$emailadmins "user@client.com,admin@client.com"
+#be sure to structure your CSV with a column header "EmailAddress" and 
+$csvname = Read-Host "Enter path to CSV"
 
-#find the OrdID
+#get the OrdID = this is also the TargetDeliveryDomain
 $orgID = (Get-OrganizationConfig).identity
 
+#list of email addresses to contact during migration process
+$emailadmins "user@client.com,admin@client.com"
+
 #Populate the migration endpoint. NOTE If there are more than one 1 you will need to adjust
-$batchname = Read-Host "Enter the migration batch name"
 $migend = Get-MigrationEndpoint | ?{$_.endpointtype -like "ExchangeRemoteMove"}
 
 #Create the Migration Batch
-$OnboardingBatch = New-MigrationBatch -Name $batchname -SourceEndpoint $midend -TargetDeliveryDomain $orgID -BadItemLimit 10 -LargeItemLimit 5 -Autostart -NotificationEmails $emailadmins -CSVData ([System.IO.File]::ReadAllBytes("C:\Users\Administrator\Desktop\RemoteOnBoarding1.csv")); 
+$batchname = Read-Host "Enter the migration batch name"
+$OnboardingBatch = New-MigrationBatch -Name $batchname -SourceEndpoint $midend -TargetDeliveryDomain $orgID -BadItemLimit 10 -LargeItemLimit 5 -Autostart -NotificationEmails $emailadmins -CSVData ([System.IO.File]::ReadAllBytes($csvname)); 
 Start-MigrationBatch -Identity $OnboardingBatch.Identity
 
 #Run the following to output migration batch status
